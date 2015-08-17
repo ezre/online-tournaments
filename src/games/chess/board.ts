06607e3b4da5f2483f6130858ff3005f206b6122
Adam Urban <adam.karol.urban@gmail.com>
2015-08-17 22:33:59 +0200

    Added initial pieces creation to board

--- src/games/chess/board.ts
+++ src/games/chess/board.ts
@@ -1,14 +1,52 @@
-import Piece = require('pieces/piece');
+import Game     = require('../game');
+import Piece    = require('pieces/piece');
 import Position = require('../position');
+import Pawn     = require('pieces/pawn');
+import Knight   = require('pieces/knight');
+import Bishop   = require('pieces/bishop');
+import King     = require('pieces/king');
+import Queen    = require('pieces/queen');
+import Rook     = require('pieces/rook');
 
 class Board {
   static size = { x: 8, y: 8 };
   private _pieces: Piece[];
+  game: Game;
   
-  constructor() {
-    for (var index = 0; index < 8; index++) {
-      // this.addPiece(new )
+  constructor(game: Game) {
+    for (var player = 0; player <= 1; player++) {
+      for (var row = player ? 6 : 0, maxRow = row + 1; row <= maxRow; row ++) {
+        for (var column = 0; column < 8; column++) {
+          this.addPieceByPosition({ x: column, y: row });
+        }
+      }
     }
+  }
+  
+  addPieceByPosition(position: Position) {
+    var piece = null;
+    
+    if (!this.isInRange(position)) {
+      return false;
+    }
+    
+    if (position.y == 1 || position.y == 6) { 
+      piece = new Pawn(position);
+    } else {
+      if (position.x % 7 == 0) {
+        piece = new Rook(position);
+      } else if (position.x % 5 == 1) {
+        piece = new Knight(position);
+      } else if (position.x % 3 == 2) {
+        piece = new Bishop(position);
+      } else if (position.x == 3) {
+        piece = new King(position);
+      } else {
+        piece = new Queen(position);
+      }
+    }
+    
+    
   }
   
   addPiece(piece: Piece) {
@@ -20,7 +58,7 @@ class Board {
   removePiece(piece: Piece) {
     var idx = this._pieces.indexOf(piece);
     
-    if(idx > -1) {
+    if (idx > -1) {
       this._pieces.slice(idx, 1);
     }
   }
