34b22ea214ef4e4398c1e761b524e5fc86b6d6b6
Adam Urban <adam.karol.urban@gmail.com>
2015-09-04 23:44:47 +0200

    Added move piece method to chess game

--- src/games/chess/index.ts
+++ src/games/chess/index.ts
@@ -2,13 +2,26 @@
 
 import Game = require('../game');
 import Board = require('./board');
+import Position = require('../position');
+import Player = require('../player');
 
 class Chess extends Game {
   board: Board;
+  maxPlayers = 2
   
   constructor() {
     super();
     this.board = new Board(this);
+  }
+  
+  movePiece(player: Player, posFrom: Position, posTo: Position) {
+    if(this.hasPlayer(player)) {
+      var piece = this.board.getPieceByPosition(posFrom);
+      
+      if(piece !== null && piece.getPlayer() === player) {
+        this.board.movePiece(piece, posTo);
+      }
+    }
   }
 }
 
