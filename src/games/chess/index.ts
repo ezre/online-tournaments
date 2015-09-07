a16f5a9710be4a6ae2d16f22cc6df54a966ffcff
Adam Urban <adam.karol.urban@gmail.com>
2015-09-07 23:40:23 +0200

    Added more move condition checks and fixed removePlayer method type'o

--- src/games/chess/index.ts
+++ src/games/chess/index.ts
@@ -1,9 +1,12 @@
 /// <reference path="../../../typings/tsd.d.ts"/>
 
-import Game = require('../game');
-import Board = require('./board');
+import Game     = require('../game');
+import Board    = require('./board');
 import Position = require('../position');
-import Player = require('../player');
+import Player   = require('../player');
+import Piece    = require('./pieces/piece');
+import King     = require('./pieces/king');
+import Rook     = require('./pieces/rook');
 
 class Chess extends Game {
   board: Board;
@@ -26,7 +29,7 @@ class Chess extends Game {
     return this;
   }
   
-  removePLayer(player: Player) {
+  removePlayer(player: Player) {
     super.removePlayer(player);
     
     if(this._players.isEmpty()) {
@@ -35,14 +38,29 @@ class Chess extends Game {
   }
   
   movePiece(player: Player, posFrom: Position, posTo: Position) {
-    if(this.hasPlayer(player)) {
+    if (this.hasPlayer(player)) {
       var piece = this.board.getPieceByPosition(posFrom);
-      
-      if(piece !== null && piece.getPlayer() === player) {
-        this.board.movePiece(piece, posTo);
+      if (piece !== null && piece.getPlayer() === player) {
+        var pieceTo = this.board.getPieceByPosition(posTo);
+        if (pieceTo === null) {
+          this.board.movePiece(piece, posTo);
+        } else {
+          if (pieceTo.player === player) {
+            this.isCastling(player, piece, pieceTo);
+          }
+        }
       } else {
         return false;
       }
+    }
+  }
+  
+  isCastling(player: Player, pieceMoved: Piece, pieceTo: Piece) {
+    if (pieceMoved.hasMoved === false && pieceTo.hasMoved === false &&
+    ((pieceMoved instanceof King && pieceTo instanceof Rook) ||
+    (pieceTo instanceof King || pieceMoved instanceof Rook)) &&
+    /* @todo Check if all fields between are empty */) {
+      
     }
   }
 }
