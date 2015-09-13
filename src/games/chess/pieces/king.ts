e64fe48879211d5c39f09ee08dec119c1c5794b5
Adam Urban <adam.karol.urban@gmail.com>
2015-09-13 23:42:53 +0200

    Added is in check and is in checkmate methods

--- src/games/chess/pieces/king.ts
+++ src/games/chess/pieces/king.ts
@@ -24,8 +24,60 @@ class King extends Piece {
     return !this.hasDoneCastling && !this.hasMoved;
   }
   
+  isPositionInCheck(position: Position) {
+    var opponent, opponentPieces, inCheck = false;
+
+    opponent = this.board.game.getFirstOpponent(this.player);
+    opponentPieces = opponent.getPieces();
+    this.inCheck = false;
+    
+    for (var i = 0; i < opponentPieces.length; i++) {
+      if (opponent.validateCapture(this._position)) {
+        this.inCheck = true;
+      }
+    }
+    
+    return this.inCheck;
+  }
+  
   isInCheck() {
-    var opponent = this.board.game.getFirstOpponent(this.player);
+    this.inCheck = this.isPositionInCheck(this.position);
+    
+    return this.inCheck;
+  }
+  
+  isInCheckMate() {
+    var positions = [], posCoords, checkCounter = 0;
+    
+    if (!this.isInCheck()) {
+      return false;
+    }
+    
+    posCoords = [
+      [-1,  1],
+      [ 0,  1],
+      [ 1,  1],
+      [-1,  0],
+      [ 1,  0],
+      [-1, -1],
+      [ 0, -1],
+      [ 1, -1]
+    ];
+    
+    for (var i = 0; i < posCoords.length; i++) {
+      var position = {
+        x: this.position.x + posCoords[i][0],
+        y: this.position.y + posCoords[i][1]
+      };
+      
+      if (this.board.isInRange(position) &&
+      this.board.getPieceByPosition(position) === null &&
+      !this.isPositionInCheck(position)) {
+        return false;
+      }
+    }
+    
+    return true;
   }
 }
 
