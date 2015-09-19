dcac327f5c71763ef98015418e57f0ae44e3a7eb
Adam Urban <adam.karol.urban@gmail.com>
2015-09-19 20:14:07 +0200

    Added some basic api messages

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,17 @@ var express = require('express');
 var app = express();
 var router = express.Router()
 
-router.get('/', function(req, res) {
+router.get('/', function(req, res, next) {
   res.send('Hello world!');
+  next();
+});
+
+router.post('/game/start', function(req, res, next) {
+  // @todo start new game here
+});
+
+router.post('/move', function (req, res, next) {
+  // @todo make a move here
 });
 
 var server = app.listen(3000, function() {
