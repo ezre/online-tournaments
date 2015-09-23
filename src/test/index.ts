f3fce62bdf49dbc80e59de38a73800bf75069306
Adam Urban <adam.karol.urban@gmail.com>
2015-09-23 23:45:02 +0200

    Added basic test file

---
+++ src/test/index.ts
@@ -0,0 +1,14 @@
+import chai   = require('chai');
+import mocha  = require('mocha');
+import io     = require('socket.io-client');
+
+var should    = chai.should();
+var socketUrl = 'http://0.0.0.0:3000'
+var options   = {
+  transports: ['websocket'],
+  'force new connection': true
+}
+
+describe('Chess server', function() {
+  
+});
