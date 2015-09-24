548f6d54009491b6ac869a2ba5e8b6b03532b66f
Adam Urban <adam.karol.urban@gmail.com>
2015-09-24 22:17:59 +0200

    Added type referenses

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,3 +1,7 @@
+/// <reference path="../../typings/requirejs/require.d.ts"/>
+/// <reference path="../../typings/mocha/mocha.d.ts"/>
+/// <reference path="../../typings/chai/chai.d.ts"/>
+
 import chai   = require('chai');
 import mocha  = require('mocha');
 import io     = require('socket.io-client');
